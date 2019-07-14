b2ddedc5ef78600785816fdec036fd0b95b96e03
NampyoJeong <jungnampyo@gmail.com>
2019-07-14 19:39:03 +0900

    requestPermission 함수 추가

--- core/src/main/java/io/npc/support/utils/Utils.kt
+++ core/src/main/java/io/npc/support/utils/Utils.kt
@@ -1,12 +1,16 @@
 package io.npc.support.utils
 
+import android.app.Activity
 import android.content.Context
+import android.content.pm.PackageManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.annotation.StringRes
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 
 fun Context.toast(message: String) {
@@ -23,6 +27,26 @@ fun Fragment.toast(@StringRes resId: Int) = context?.toast(resId)
 
 fun ViewGroup.inflate(@LayoutRes res: Int): View {
     return LayoutInflater.from(context).inflate(res, this, false)
+}
+
+fun Activity.requestPermission(permission: String, requestCode: Int, onSuccess: () -> Unit) {
+    if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+        ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
+    } else {
+        onSuccess()
+    }
+}
+
+fun Fragment.requestPermission(permission: String, requestCode: Int, onSuccess: () -> Unit) {
+    if (ContextCompat.checkSelfPermission(requireContext(), permission) != PackageManager.PERMISSION_GRANTED) {
+        requestPermissions(arrayOf(permission), requestCode)
+    } else {
+        onSuccess()
+    }
+}
+
+fun isGrantedPermission(grantResults: IntArray): Boolean {
+    return grantResults.getOrNull(0) == PackageManager.PERMISSION_GRANTED
 }
 
 fun String?.int(): Int = this?.toInt() ?: 0
