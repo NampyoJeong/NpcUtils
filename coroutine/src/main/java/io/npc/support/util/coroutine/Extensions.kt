74e4c7f755ba88b5e21f7d2c2015eacee840fbe6
NampyoJeong <jungnampyo@gmail.com>
2019-07-25 22:59:05 +0900

    Add OnTextChangeListener and extension

--- coroutine/src/main/java/io/npc/support/util/coroutine/Extensions.kt
+++ coroutine/src/main/java/io/npc/support/util/coroutine/Extensions.kt
@@ -1,7 +1,16 @@
 package io.npc.support.util.coroutine
 
+import android.text.Editable
 import android.view.View
+import android.widget.EditText
 
 fun View.setOnSingleClickListener(timeMillis: Long = 500, listener: (view: View?) -> Unit) {
     setOnClickListener(OnSingleClickListener(timeMillis, listener))
+}
+
+fun EditText.setOnTextChangeListener(timeMillis: Long = 200, afterTextChanged: (s: Editable?) -> Unit) {
+    addTextChangedListener(OnTextChangeListener(
+        afterTextChanged = afterTextChanged,
+        timeMillis = timeMillis
+    ))
 }
