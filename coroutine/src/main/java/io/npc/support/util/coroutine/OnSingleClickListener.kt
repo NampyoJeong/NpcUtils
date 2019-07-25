871690de6a88edb2e7f14418b5e633a4c0f2a8c5
NampyoJeong <jungnampyo@gmail.com>
2019-07-25 22:47:13 +0900

    OnSingleClickListener에서 GlobalScope를 CoroutineScope로 변경
    - ObsoleteCoroutinesApi annotation 추가

--- coroutine/src/main/java/io/npc/support/util/coroutine/OnSingleClickListener.kt
+++ coroutine/src/main/java/io/npc/support/util/coroutine/OnSingleClickListener.kt
@@ -1,14 +1,16 @@
 package io.npc.support.util.coroutine
 
 import android.view.View
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.ObsoleteCoroutinesApi
 import kotlinx.coroutines.channels.actor
 import kotlinx.coroutines.delay
 
+@ObsoleteCoroutinesApi
 class OnSingleClickListener(private val timeMillis: Long = 500, private val click: (view: View?) -> Unit) : View.OnClickListener {
 
-    private val actor = GlobalScope.actor<View?>(Dispatchers.Main) {
+    private val actor = CoroutineScope(Dispatchers.Main).actor<View?> {
         for (event in channel) {
             click(event)
             delay(timeMillis)
